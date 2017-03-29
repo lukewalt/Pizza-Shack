2eb166bc1f05f78c0fd0e9750ce68e7dc14c9c9d
Luke Woodward <luke.wdwrd@gmail.com>
2017-03-29 15:10:19 -0500

    craeted background color and added img

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// configure pug our view engine
+app.set('view engine', 'pug')
+
+// MIDDLEWARE(s)
+
+app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
+
 const port = process.env.PORT || 3000;
 
-//establishes 
+//establishes
 app.listen(port, () => {
   console.log(`YOU ARE LISTENING ON PORT ${port}`);
 })
